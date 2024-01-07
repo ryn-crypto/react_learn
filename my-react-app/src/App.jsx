f3505915f03686588b852dc62fc51608d4365d59
Riyan First Tiyanto <riyanfirst13@gmail.com>
2024-01-07 21:20:20 +0700

    learn: React

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <h1 className="text-white">My React App</h1>
+    </div>
+  );
+}
+
+export default App;
