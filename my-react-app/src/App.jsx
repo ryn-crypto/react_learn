e040c5e6a5d994d48f2486af9d4e19a1c9a3dd42
Riyan First Tiyanto <riyanfirst13@gmail.com>
2024-01-10 00:17:21 +0700

    learn: atomic components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,14 @@
+import React from "react";
+import Button from "./components/Elements/Button";
+
 function App() {
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <h1 className="text-white">My React App</h1>
+    <div className="flex justify-center bg-slate-400 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Logout</Button>
+        <Button>gak reti lek</Button>
+      </div>
     </div>
   );
 }
---
+++ my-react-app/src/components/Elements/Button/index.jsx
@@ -0,0 +1,13 @@
+const Button = (props) => {
+  const { children, variant = "bg-black" } = props;
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
